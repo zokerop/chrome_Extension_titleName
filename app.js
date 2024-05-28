958fcfb5bfc2b1931111f2766a6baa688462b4d4
Zoker <99017614+zokerop@users.noreply.github.com>
2024-05-28 23:28:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById('titleBtn').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      let activeTab = tabs[0];
+      let title = activeTab.title;
+      document.getElementById('titleName').innerHTML = title;
+    });
+  });
+
+
+  // document.getElementById('titleBtn').addEventListener('click', () => {
+  //   let tab =  chrome.tabs.query({ active: true, currentWindow: true });
+  //   console.log(tab);
+  //  });
